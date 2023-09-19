2c186a4d4de0917341a3623a7f356ce16b010fbd
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-09-19 21:55:50 +0100

    The diff shows changes in the App.js and ImageCard.js files. In App.js, the loading state is now displayed as a large centered heading, and the images are now rendered in a grid. In ImageCard.js, the image's tags are now displayed as a list of inline elements, and the user who took the photo is now displayed.

--- src/App.js
+++ src/App.js
@@ -31,7 +31,10 @@ function App() {
       />
 
       {isLoading ? (
-        <p>Loading...</p>
+        <h1 className="text-6xl text-center mx-auto mt-32">
+          Loading...
+        </h1>
+        
       ) : (
         <div className="grid grid-cols-3 gap-4">
           {images.map((image) => (
--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,33 +1,40 @@
 import React from 'react'
 
 const ImageCard = ({image}) => {
+    const tags= image.tags.split(',');
+
   return (
     <div className=" max-w-sm rounded overflow-hidden shadow-lg">
       <img src={image.webformatURL} alt="" className="w-full" />
       <div className="px-6 py-4">
-        <div className="font-bold text-black-500 mb-2">The Coldest Sunset</div>
+        <div className="font-bold text-black-500 mb-2">
+          TPhoto by{image.user}
+        </div>
         <ul>
           <li className="text-black-700 text-base">
-            <strong>Views:</strong>3333
+            <strong>Views:</strong>
+            {image.views}
           </li>
           <li className="text-black-700 text-base">
-            <strong>Downloads:</strong>33
+            <strong>Downloads:</strong>
+            {image.downloads}
           </li>
           <li className="text-black-700 text-base">
-            <strong>Likes:</strong>3
+            <strong>Likes:</strong>
+            {image.likes}
           </li>
         </ul>
       </div>
       <div className="px-6 py-4">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-black-700 mr-2">
-          #tag1
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-black-700 mr-2">
-          #tag2
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-black-700 mr-2">
-          #tag3
-        </span>
+        {tags.map((tag, index) => (
+          <span
+            key={index}
+            className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
+          >
+            #{tag}
+          </span>
+        ))}
+        
       </div>
     </div>
   );
