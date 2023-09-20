cc08e9c44c921ecc17afe43d5ab8233044d5515a
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-09-20 11:04:35 +0100

    Update App.js and ImageCard.js

--- src/App.js
+++ src/App.js
@@ -26,7 +26,6 @@ function App() {
       {!isLoading && images.length === 0 && (
         <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1>
       )}
-
       {isLoading ? (
         <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1>
       ) : (
